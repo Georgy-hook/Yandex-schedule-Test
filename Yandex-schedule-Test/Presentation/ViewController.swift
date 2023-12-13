e5ef56809f817ed0aa7ca5179c9069e59eb6ed80
Georgy <georgin2000155@gmail.com>
2023-12-13 21:35:53 +0300

    - Initial configuration

---
+++ Yandex-schedule-Test/Presentation/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Yandex-schedule-Test
+//
+//  Created by Georgy on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        addSubviews()
+        applyConstraints()
+    }
+}
+
+
+// MARK: - Layout
+extension ViewController {
+    private func configureUI() {
+        
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func applyConstraints() {
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
